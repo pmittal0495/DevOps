6df01ec6afc219134df48e882ebeff27817a30d2
pmittal0495 <39042801+pmittal0495@users.noreply.github.com>
2018-05-07 15:40:15 +0530

    Update Hello.java

--- DemoTest/src/com/solution/Hello.java
+++ DemoTest/src/com/solution/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World ");
 		System.out.println("My Name Is Prashant Mittal ");
+		System.out.println("My last Name Is Mittal ");
 	}
 
 }
