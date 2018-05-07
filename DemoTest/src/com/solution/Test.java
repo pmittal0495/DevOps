1d1044fdd6a1ed6a862c3c808365b699004452b5
pramitta <pramitta@DIN55001702.corp.capgemini.com>
2018-05-07 14:31:22 +0530

    Hello World

---
+++ DemoTest/src/com/solution/Test.java
@@ -0,0 +1,33 @@
+package com.solution;
+
+import java.util.ArrayList;
+
+public class Test {
+
+public static void main(String[] args) {
+		
+		Solution solution= new Solution();
+		 
+		main(null);
+	}
+}
+
+
+ArrayList<int[]> arl=new ArrayList<int[]>();
+int a1[]={1,2,3};
+arl.add(0,a1);
+System.out.println("Arraylist contains:"+arl.get(0));
+
+
+//int[]  A = {1,3,6,4,1,2};
+	int A[]={1,3,6,4,1,2};
+	
+	ArrayList<int[]> arl=new ArrayList<int[]>();
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Arraylist contains:"+arl);
+	}
