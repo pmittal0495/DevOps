193130ec103ba0adc57e7a9addc1ff36bd55451f
pramitta <pramitta@DIN55001702.corp.capgemini.com>
2018-05-07 14:42:08 +0530

    updated hello

--- DemoTest/src/com/solution/Hello.java
+++ DemoTest/src/com/solution/Hello.java
@@ -3,6 +3,7 @@ package com.solution;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World ");
+		System.out.println("My Name Is Prashant Mittal ");
 	}
 
 }
